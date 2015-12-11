eb93cff884a6594417a5b4b496417fee72ac60d2
morganhowell95 <morgan.howell95@gmail.com>
2015-12-11 15:05:54 -0500

    adding privacy to git repos

--- app/controllers/usercodes_controller.rb
+++ app/controllers/usercodes_controller.rb
@@ -4,7 +4,7 @@ class UsercodesController < ApplicationController
   # GET /usercodes
   # GET /usercodes.json
   def index
-    @usercodes = Usercode.all
+    @usercodes = Usercode.where(user_id: session[:user_id])
   end
 
   # GET /usercodes/1
